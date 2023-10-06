84ca18fe9112c2a932e14df13358ac2672f799b7
Daniel163and <139417116+Daniel163and@users.noreply.github.com>
2023-10-05 19:48:02 -0500

    proyecto final

--- numprimo.c
+++ numprimo.c
@@ -1,24 +1,25 @@
- #include <stdio.h>
-
- int main (void) {
-
-    int num, i, numprimo = 1;
+#include <stdio.h>
+int main() {
+    int num, i, numprimo = 0;
 
     printf("Ingrese un numero entero: ");
     scanf("%d", &num);
 
-    for(i = 2; i <= num / 2; ++i) {
-        if(num % i == 0) {
-            numprimo = 0;
+    for(i = 2; i <= num/2; ++i) {
+        if(num%i == 0) {
+            numprimo = 1;
             break;
         }
-
-        if (numprimo == 1)
-        printf("%d es un numero primo.", num);
+    }
+    if (num == 1) {
+        printf("1 no es primo.");
+    }
+    else {
+        if (numprimo == 0)
+            printf("%d es un numero primo.", num);
         else
-        printf("%d no es un numero primo.", num);
-
-        printf("gracias por usar el programa");
-
+            printf("%d no es un numero primo.", num);
+    }
+    
     return 0;
- }
+}
