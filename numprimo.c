b419baa56d71b112cf88b6b85d7855e3f661ebb7
Daniel163and <139417116+Daniel163and@users.noreply.github.com>
2023-10-05 19:09:19 -0500

    primer respaldo

---
+++ numprimo.c
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+
+ int main (void) {
+
+    int num, i, numprimo = 1;
+
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &num);
+
+    for(i = 2; i <= num / 2; ++i) {
+        if(num % i == 0) {
+            numprimo = 0;
+            break;
+        }
+    
+
+    return 0;
+ }
