164cc0fa85c0c76895d59b0a388fa20baa49cd63
Andradeudla <147015554+Andradeudla@users.noreply.github.com>
2023-10-12 22:32:32 -0500

    primer cambio

---
+++ gauss.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int num, i, suma = 0;
+    
+    do {
+        printf("Ingrese un número entero positivo: ");
+        scanf("%d", &num);
+
+        if(num < 0) {
+            printf("Debe ingresar un número positivo.\n");
+        }
+    } while(num < 0);
+    
+    return 0;
+}
