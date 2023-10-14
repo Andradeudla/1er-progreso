b4d25d4d675a00dbed52fcb9eb03c1bfe4bf5f9d
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-10-13 22:14:47 -0500

    segundo commit

--- sumagauss.c
+++ sumagauss.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main() {
+    int num, i, suma = 0;
+
+    do
+    {
+        printf("Ingrese un número entero positivo: ");
+        scanf("%d", &num);
+    } while (num < 0);
     
     return 0;
 }
