5764ec84e7b5883303daa4dcfa24a69832e8708a
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-10-15 12:02:58 -0500

    segundo commit

--- ejercicio4.c
+++ ejercicio4.c
@@ -4,6 +4,12 @@ int main() {
 
     printf("Introduce el primer número(ingrese un numero positivo): ");
     scanf("%d", &num1);
+            
+    if(num1 < 0) {
+        printf("Debe ingresar un número positivo.\n");
+        }
+    printf("Introduce el segundo número: ");
+    scanf("%d", &num2);
     
     return 0;
 }
