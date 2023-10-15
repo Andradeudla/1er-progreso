e72be4f3cb247b25e2deb513ab636f649b4dc71c
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-10-15 13:00:11 -0500

    tercer commit

--- ejercicio24.c
+++ ejercicio24.c
@@ -5,9 +5,14 @@ int main (){
     
      printf("Introduce un número entero: ");
     scanf("%d", &num);
-    
+
     while(num != 0) {
         digito = num % 10;
-
+        if(digito % 2 == 0) {
+            suma += digito;
+        }
+        num /= 10;
+    }
+    
     return 0;
 }
