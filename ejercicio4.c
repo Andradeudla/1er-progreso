f673f28e74de189596246fa03bbc6f8223c7d25a
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-10-15 12:08:46 -0500

    tercer commit

--- ejercicio4.c
+++ ejercicio4.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 int main() {
     int num1, num2, i;
-
+    
+    do
+    {
+    
     printf("Introduce el primer número(ingrese un numero positivo): ");
     scanf("%d", &num1);
             
     if(num1 < 0) {
         printf("Debe ingresar un número positivo.\n");
         }
+    } while (num1 < 0);
+
+    do
+    {
     printf("Introduce el segundo número: ");
     scanf("%d", &num2);
     
+    if(num2 < 0) {
+        printf("Debe ingresar un número positivo.\n");
+        }
+
+    } while (num2 < 0);
+
     return 0;
 }
