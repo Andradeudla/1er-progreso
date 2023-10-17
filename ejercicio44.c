599699420a3913126b612ebb2d3e08f7b021c456
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-10-16 21:22:23 -0500

    segundo commit

--- ejercicio44.c
+++ ejercicio44.c
@@ -5,5 +5,8 @@ int main (){
     printf("Ingrese un numero: ");
     scanf("%d", &numero);
 
+    if (numero < 0)
+        printf("Numero equivocado, ingrese un numero positivo\n");
+
     return 0;
 }
